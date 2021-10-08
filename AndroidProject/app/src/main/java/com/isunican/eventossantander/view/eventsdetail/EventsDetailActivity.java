cf61002e57fec086ad1e0b6cf24095bfea680972
pag804 <pag804@alumnos.unican.es>
2021-10-08 12:33:50 +0200

    Agregada la lógica de negocio de verInfoDetalle. Falta añadir la lógica del mapView.

--- AndroidProject/app/src/main/java/com/isunican/eventossantander/view/eventsdetail/EventsDetailActivity.java
+++ AndroidProject/app/src/main/java/com/isunican/eventossantander/view/eventsdetail/EventsDetailActivity.java
@@ -1,14 +1,24 @@
 package com.isunican.eventossantander.view.eventsdetail;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.isunican.eventossantander.R;
 import com.isunican.eventossantander.model.Event;
 import com.squareup.picasso.Picasso;
@@ -22,9 +32,19 @@ public class EventsDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_detail);
 
+        ActionBar actBar = getSupportActionBar();
+
+        actBar.setDisplayHomeAsUpEnabled(true); // Botón home en la barra superior
+        actBar.setDisplayShowHomeEnabled(true);
+
         // Link to view elements
         TextView eventTitleText = findViewById(R.id.event_detail_title);
         TextView eventDateText = findViewById(R.id.event_detail_date);
+        TextView eventDescripText = findViewById(R.id.event_detail_descrip);
+        ImageView eventImage = findViewById(R.id.event_detail_Image);
+        Button eventButton = findViewById(R.id.event_detail_MasInfo);
+
+        //GoogleMap googleMap = ((MapView) findViewById(R.id.event_detail_mapView)).getMap();
 
         // Get Event from the intent that triggered this activity
         Event event = getIntent().getExtras().getParcelable(INTENT_EVENT);
@@ -32,5 +52,19 @@ public class EventsDetailActivity extends AppCompatActivity {
         // Set information
         eventTitleText.setText(event.getNombre());
         eventDateText.setText(event.getFecha());
+        eventDescripText.setText(Html.fromHtml(event.getDescripcion()));
+        eventDescripText.setMovementMethod(LinkMovementMethod.getInstance());
+        Picasso.get().load(event.getImagen()).into(eventImage);
+        eventImage.setAdjustViewBounds(true);
+        eventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(event.getEnlace()));
+                startActivity(browserIntent);
+            }
+        });
+        //googleMap.addMarker(new MarkerOptions().position(new LatLng( event.getLatitud(), -event.getLongitud())).title("Marker"));
+
     }
+
 }
