5a85d13457a29b5096870a9c524524a2f040444c
pag804 <pag804@alumnos.unican.es>
2021-11-03 16:57:54 +0100

    Solucionado bloque de código duplicado en la clase AccessSharedPrefs

--- AndroidProject/app/src/main/java/com/isunican/eventossantander/utils/AccessSharedPrefs.java
+++ AndroidProject/app/src/main/java/com/isunican/eventossantander/utils/AccessSharedPrefs.java
@@ -3,32 +3,27 @@ package com.isunican.eventossantander.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-public class AccessSharedPrefs implements ISharedPrefs{
+public class AccessSharedPrefs implements ISharedPrefs {
 
     private static final String KEY_CATEGORIAS = "CATEGORIAS";
 
-    private Context c;
+    private final SharedPreferences sharedPref;
+    private final SharedPreferences.Editor editor;
 
     public AccessSharedPrefs(Context c){
-        this.c = c;
+        sharedPref = c.getSharedPreferences(KEY_CATEGORIAS, Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
     }
 
     @Override
     public Set<String> getSelectedCategories() {
-        SharedPreferences sharedPref = c.getSharedPreferences(KEY_CATEGORIAS, Context.MODE_PRIVATE);
-        Set<String> categorias = sharedPref.getStringSet(KEY_CATEGORIAS, null);
-        return categorias;
+        return sharedPref.getStringSet(KEY_CATEGORIAS, null);
     }
 
     @Override
     public void setSelectedCategories(Set<String> categorias) {
-        SharedPreferences sharedPref = c.getSharedPreferences(KEY_CATEGORIAS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
         editor.clear();
         editor.putStringSet(KEY_CATEGORIAS, categorias);
         editor.apply();
@@ -36,8 +31,6 @@ public class AccessSharedPrefs implements ISharedPrefs{
 
     @Override
     public void clearCategories() {
-        SharedPreferences sharedPref = c.getSharedPreferences(KEY_CATEGORIAS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
         editor.clear();
         editor.apply();
     }
